dbbac424c88176891f0ac3c8273bb7cc870b2d9b
Kelvin Murimi Njeru <148711587+kelvoBraavos@users.noreply.github.com>
2024-09-19 12:38:30 +0300

    Version 2

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <body>
     <section class="congratulations">
         <h1>Congratulations!</h1>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
+        <p>Your AWS Elastic Beanstalk v2 <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version</p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
     </section>
